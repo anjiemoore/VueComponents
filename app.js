4a6546c5d877a8811546603cdb75720960d82dd7
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-10-12 11:08:58 -0600

    made a functioning component

--- app.js
+++ app.js
@@ -4,17 +4,44 @@ const app = Vue.createApp({
             friends: [{
                 id: 'manuel',
                 name: 'Manuel Lorenz',
-                phone: '01234 5678 991',
+                phone: '124 678 991',
                 email: 'manuel@localhost.com'
             },
             {
                 id: 'julie',
                 name: 'Julie Jones',
-                phone: '0986 543 221',
+                phone: '986 543 221',
                 email: 'julie@localhost.com'
             }]
         }
-    }
-
+    },
 })
+//component needs an identifier, needs a dash
+app.component('friend-contact', {
+    template: 
+    `<li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+</li>`,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '124 678 991',
+                email: 'manuel@localhost.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+});
 app.mount('#app')
